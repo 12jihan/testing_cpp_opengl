70e8ba519bfb8d4b9399c6c7500ee7c29fbb3d4f
Jareem <Jareem.X.Hoff.-ND@abc.com>
2021-06-28 03:00:23 -0400

    successfully made the triangle that shows that everything I did worked properly

--- src/main.cpp
+++ src/main.cpp
@@ -33,21 +33,31 @@ int main(void)
     // updates with the version once it passes:
     std::cout << glGetString(GL_VERSION) << std::endl;
 
-    // Define a vertex buffer:
+    // Float Array:
+    float positions[6] = {
+       -0.5f, -0.5f,
+        0.0f,  0.5f,
+        0.5f, -0.5f
+    };
+
+    // Define a vertex buffer (buffer of memory saved on the gpu~):
     unsigned int buffer;
     glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
 
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+    
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
-
-        glBegin(GL_TRIANGLES);
-        glVertex2f(-0.5f, -0.5f);
-        glVertex2f(0.0f, 0.5f);
-        glVertex2f(0.5f, -0.5f);
-        glEnd();
+        
+        // Draws the triangles:
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
